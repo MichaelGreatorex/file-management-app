7315e417591df721a14489b23659a209307e8b61
Michael Greatorex <magreatorex@gmail.com>
2024-07-22 14:32:44 +0100

    latest

---
+++ public/NavBarGenerator.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to generate the navbar content
+    function generateNavbar() {
+        const navBar = document.getElementById('navBar');
+
+    }
+    
+    // Generate the sidebar on page load
+    generateNavbar();
+    });
+    
+    
